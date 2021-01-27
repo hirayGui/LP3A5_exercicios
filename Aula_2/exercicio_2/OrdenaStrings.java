b8c189e818dacfa82e24033ed549bdc4fee2d856
Guilherme Brito <hiraygui@gmail.com>
2021-01-26 21:13:59 -0300

    adicionando exercicio 2

---
+++ Aula_2/exercicio_2/OrdenaStrings.java
@@ -0,0 +1,61 @@
+import java.util.ArrayList;
+/*import java.util.Collections;
+import java.util.Comparator;*/
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+
+	public static void main(String[] args) {
+
+		List<String> palavras = new ArrayList<String>();
+
+		palavras.add("alura online");
+		palavras.add("editora casa do codigo");
+		palavras.add("caelum");
+
+		palavras.sort((s1, s2) -> {
+
+			if (s1.length() < s2.length())
+				return -1;
+
+			if (s1.length() > s2.length())
+				return 1;
+
+			return 0;
+		});
+
+		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
+
+		System.out.println(palavras);
+
+		Consumer<String> impressor = s -> System.out.println(s);
+		palavras.forEach(impressor);
+
+		// for (String p : palavras)
+		// System.out.println(p);
+
+		palavras.forEach(s -> System.out.println(s));
+
+	}
+}
+
+/*
+ * class ImprimeNaLinha implements Consumer<String> {
+ * 
+ * @Override public void accept(String s) { System.out.println(s); }
+ * 
+ * }
+ */
+
+/*
+ * class ComparadorPorTamanho implements Comparator<String> {
+ * 
+ * @Override public int compare(String s1, String s2) {
+ * 
+ * if (s1.length() < s2.length()) return -1;
+ * 
+ * if (s1.length() > s2.length()) return 1;
+ * 
+ * return 0; } }
+ */
